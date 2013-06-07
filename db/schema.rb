a7fe3e2b433e649bdc8ca594dd2656b84bf905d5
Christopher Cruz <chris1cruz@gmail.com>
2013-06-07 16:27:28 -0500

    removed devise migration, moved into users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604133531) do
+ActiveRecord::Schema.define(:version => 20130524153448) do
 
   create_table "admins", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -110,14 +110,8 @@ ActiveRecord::Schema.define(:version => 20130604133531) do
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
-
-  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
 end
