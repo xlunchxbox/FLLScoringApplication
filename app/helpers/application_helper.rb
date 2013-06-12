45aa8eed904d0d769388a78a667435687f2f9a9e
Christopher Cruz <chris1cruz@gmail.com>
2013-06-12 16:37:00 -0500

    fixed tool tips on all tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}   
+    
   end
   
   def sortableCV(column, title = nil)
@@ -25,5 +26,11 @@ module ApplicationHelper
     css_class = column == sort_columnP ? "current #{sort_direction}" : nil
     direction = column == sort_columnP && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+  
+  def tooltip(content, options = {}, html_options = {}, *parameters_for_method_reference) 
+    html_options[:title] = options[:tooltip] 
+    html_options[:rel] = html_options[:rel] || 'tooltip' 
+    content_tag("span", content, html_options)
   end
 end
