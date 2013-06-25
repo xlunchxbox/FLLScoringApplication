3e4a97fa5a49288b9accfe512d4b02e8c58b5d7f
Christopher Cruz <chris1cruz@gmail.com>
2013-06-25 08:21:50 -0500

    production.rb error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-FLLApplication::Application.configure do
+FLLScoringApplication::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
