749837ba7d38f46a12a70c9a49d373dd413dceca
Christopher Cruz <chris1cruz@gmail.com>
2013-07-02 13:14:41 -0500

    fixes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,14 +13,14 @@ class SearchController < ApplicationController
 #     @search_robot_design = RobotDesign.where(search_string_robot_design)
 #   end
   
-def search_results
-	search_string_core = " team_number is " + "\"" + params[:search] + "\""
-	@search_core = CoreValue.where(search_string_core)
+  def search_results
+  	search_string_core = " team_number is " + "\"" + params[:search] + "\""
+  	@search_core = CoreValue.where(search_string_core)
 
-	search_string_project = " team_number is " + "\"" + params[:search] + "\""
-	@search_project = Project.where(search_string_project)
+  	search_string_project = " team_number is " + "\"" + params[:search] + "\""
+  	@search_project = Project.where(search_string_project)
 
-	search_string_robot_design = " team_number is " + "\"" + params[:search] + "\""
-	@search_robot_design = RobotDesign.where(search_string_robot_design)
-end
+  	search_string_robot_design =  " team_number is " + "\"" + params[:search] + "\""
+  	@search_robot_design = RobotDesign.where(search_string_robot_design)
+  end
 end
