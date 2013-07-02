6c1a99cc1c9e7075363f45d541241d4b7c55398e
Christopher Cruz <chris1cruz@gmail.com>
2013-07-02 09:50:00 -0500

    PG search testing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,26 @@
 class SearchController < ApplicationController
-  def search_results
-  	search_string_core = ""
-  	search_string_core = search_string_core + " team_number is " + "\"" + params[:search] + "\""
-  	@search_core = CoreValue.where(search_string_core)
-
-  	search_string_project = ""
-  	search_string_project = search_string_project + " team_number is " + "\"" + params[:search] + "\""
-  	@search_project = Project.where(search_string_project)
-
-  	search_string_robot_design = ""
-  	search_string_robot_design = search_string_robot_design + " team_number is " + "\"" + params[:search] + "\""
-  	@search_robot_design = RobotDesign.where(search_string_robot_design)
-  end
+  # def search_results
+#     search_string_core = ""
+#     search_string_core = search_string_core + " team_number is " + "\"" + params[:search] + "\""
+#     @search_core = CoreValue.where(search_string_core)
+# 
+#     search_string_project = ""
+#     search_string_project = search_string_project + " team_number is " + "\"" + params[:search] + "\""
+#     @search_project = Project.where(search_string_project)
+# 
+#     search_string_robot_design = ""
+#     search_string_robot_design = search_string_robot_design + " team_number is " + "\"" + params[:search] + "\""
+#     @search_robot_design = RobotDesign.where(search_string_robot_design)
+#   end
   
+def search_results
+	search_string_core = " team_number is " + "\"" + params[:search] + "\""
+	@search_core = CoreValue.where(search_string_core)
+
+	search_string_project = " team_number is " + "\"" + params[:search] + "\""
+	@search_project = Project.where(search_string_project)
+
+	search_string_robot_design = " team_number is " + "\"" + params[:search] + "\""
+	@search_robot_design = RobotDesign.where(search_string_robot_design)
+end
 end
